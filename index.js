a552ff64deca5bfbe942682f97c9d6e156d6b05f
kozukimomo <momoelo1@icloud.com>
2024-02-03 21:02:11 +0100

    uibuild

--- index.js
+++ index.js
@@ -27,14 +27,12 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
-    return response.status(400).send({error: error.message})
+    return response.status(400).send({ error: error.message });
   }
 
   next(error);
 };
 
-let persons = [
-];
 
 app.get("/api", (request, response) => {
   response.send("<h1>LA ILAHA ILLA ALLAH</h1>");
@@ -54,22 +52,27 @@ app.post("/api/persons", (request, response, next) => {
       error: "missing name or number",
     });
   } else {
-    console.log('successful');
-    person.save().then((savedPerson) => {
-      response.json(savedPerson);
-    }).catch(error => next(error))
+    console.log("successful");
+    person
+      .save()
+      .then((savedPerson) => {
+        response.json(savedPerson);
+      })
+      .catch((error) => next(error));
   }
 });
 
 app.get("/api/info", (request, response, next) => {
-  Person.countDocuments(request.params.id).then(count => {
-    const date = new Date()
-    response.send(`
+  Person.countDocuments(request.params.id)
+    .then((count) => {
+      const date = new Date();
+      response.send(`
     <p>Phonebook has info for ${count} persons</p>
     <br />
     <p>${date}</p>
     `);
-  }).catch(error => next(error))
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/persons", (request, response) => {
@@ -93,21 +96,20 @@ app.get("/api/persons/:id", (request, response, next) => {
 app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then((result) => {
-      console.log('deleted', result);
+      console.log("deleted", result);
       response.status(204).end();
     })
     .catch((error) => next(error));
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+  const { name, number } = request.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatePerson) => {
       console.log("updated", updatePerson);
       response.json(updatePerson);
--- models/person.js
+++ models/person.js
@@ -15,21 +15,30 @@ mongoose
     console.log("error connecting to monogDB", error.message);
   });
 
+const phoneValidation = (validation) =>
+  /^(\d{2}-\d{8}|\d{3}-\d{8})/.test(validation);
+
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: Number,
+    validate: {
+      validator: phoneValidation,
+    },
+    required: true,
+  },
 });
 
 personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id
-    delete returnedObject.__v
+    delete returnedObject._id;
+    delete returnedObject.__v;
   },
 });
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model("Person", personSchema);
