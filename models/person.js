6d933be351d4b6f67a8f93ea1478aec128038c32
kozukimomo <momoelo1@icloud.com>
2024-02-05 22:38:01 +0100

    uibuild

--- models/person.js
+++ models/person.js
@@ -1,44 +1,46 @@
-const mongoose = require("mongoose");
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to ", url);
+console.log('connecting to ', url)
 
 mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("connecting to mongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to monogDB", error.message);
-  });
+    .connect(url)
+    .then((result) => {
+        console.log('connecting to mongoDB', result)
+    })
+    .catch((error) => {
+        console.log('error connecting to monogDB', error.message)
+    })
 
 const phoneValidation = (validation) =>
-  /^(\d{2}-\d{7}|\d{3}-\d{7}|\d{2}-\d{8}|\d{3}-\d{8})/.test(validation);
+    /^(\d{2}-\d{7}|\d{3}-\d{7}|\d{2}-\d{8}|\d{3}-\d{8})/.test(validation)
 
 const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minLength: 3,
-    required: true,
-  },
-  number: {
-    type: String,
-    validate: {
-      validator: phoneValidation,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true,
     },
-    required: true,
-  },
-});
+    number: {
+        type: String,
+        validate: {
+            validator: phoneValidation,
+        },
+        required: true,
+    },
+})
 
-personSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    },
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
--- mongo.js
+++ mongo.js
@@ -1,45 +1,31 @@
-const mongoose = require("mongoose");
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+    console.log('give password as argument')
+    process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://momoelo1:${password}@cluster0.yaowcxp.mongodb.net/personApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://momoelo1:${password}@cluster0.yaowcxp.mongodb.net/personApp?retryWrites=true&w=majority`
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-});
+    name: String,
+    number: String,
+})
 
-const Person = mongoose.model("Person", personSchema);
-
-
-
-const name = process.argv[3];
-const number = process.argv[4];
+const Person = mongoose.model('Person', personSchema)
 
 
-const person = new Person({
-  name: name,
-  number: number,
-});
-
-
-/*
-person.save().then((result) => {
-  console.log("person saved!");
-});
-*/
 
 Person.find({}).then((result) => {
-  result.forEach((person) => {
-    console.log(person);
-  });
-  mongoose.connection.close();
-});
+    result.forEach((person) => {
+        console.log(person)
+    })
+    mongoose.connection.close()
+})
